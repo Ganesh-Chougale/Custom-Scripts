30c0ace4b3f0ead3efb0f6df2c4993e56f4f2b73
gchougale <135229221+Ganesh-Chougale@users.noreply.github.com>
2025-07-12 20:23:27 +0530

    Update and rename gp (GitPush).sh to Git Commads.md

--- Sh Scripts/gp (GitPush).sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get current date and time in format: DD-MM-YYYY / HH:MM AM/PM
-TIMESTAMP=$(date +"%d-%m-%Y / %I:%M%p")
-
-git add .
-git commit -m "$TIMESTAMP"
-git push origin main
